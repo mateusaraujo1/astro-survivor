e9eda01fb28c3801bc32f9a66117a1c8155e515e
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-02-06 22:21:04 -0300

    a02: finish

---
+++ scripts/laser.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var velocidade = 200
+
+func _ready():
+	pass 
+
+
+func _process(delta):
+	translate(Vector2(0, -1) * velocidade * delta)
+	pass
--- scripts/ship.gd
+++ scripts/ship.gd
@@ -11,6 +11,7 @@ func _ready():
 func _process(delta):
 	
 	var direcao_x = 0
+	var direcao_y = 0
 	
 	# 1 seria 1 pixel
 	if Input.is_action_pressed("mv-esquerdo"):
@@ -18,6 +19,12 @@ func _process(delta):
 	# += pra poder zerar e a nave ficar parada, se as duas teclas forem pressionadas
 	if Input.is_action_pressed("mv-direito"):
 		direcao_x += 1
+		
+	if Input.is_action_pressed("mv-cima"):
+		direcao_y += -1
+		
+	if Input.is_action_pressed("mv-baixo"):
+		direcao_y += 1
 	
-	translate(Vector2(direcao_x,0) * velocidade * delta)
+	translate(Vector2(direcao_x, direcao_y) * velocidade * delta)
 	pass
