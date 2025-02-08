0fc82414e41ea75d3e441e0b5423e6b59619a753
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-02-08 19:06:46 -0300

    a07: finish

--- scripts/asteroide.gd
+++ scripts/asteroide.gd
@@ -1,9 +1,26 @@
 extends Node2D
 
+var chosen = 0
 
 func _ready():
-	pass 
-
+	randomize()
+	#deixa mais aleatório, senão iria repetir os mesmos asteroides
+	
+	for a in $asteroides.get_children():
+		a.visible = false
+	#puxa todos os filhos do grupo "asteroides" e deixa invisiveis
+	
+	chosen = (randi() % $asteroides.get_child_count()) + 1
+	#gera um numero aleatorio entre 1 e 5
+	
+	var node = get_node("asteroides/asteroid-" + str(chosen))
+	node.visible = true
+	#deixa um asteroide aleatório visível
+	
+	$area/shape.shape.radius = node.texture.get_width() / 2
+	# pega a largura da textura (diametro), divide por 2 pra pegar o raio,
+	# e faz a area shape (area de colisão) receber o mesmo raio
+	# ou seja, area de colisão recebe o tamanho da textura
 
 
 func _process(delta):
